4f0a9424c1b01a71536b40466863a4cb3b9c7159
Manisha Malik <manishamalik11@gmail.com>
2018-11-01 15:54:05 -0700

    commit 2

--- BSTMain.java
+++ BSTMain.java
@@ -15,12 +15,6 @@ public class BSTMain {
         insert.insertElement(root,2);
         insert.insertElement(root,10);
         BSTNode node= insert.insertElement(root,5);
-//        insert.printInorderBSTTree(node);
-//        BSTSearch bstSearch =new BSTSearch();
-//        System.out.println(" "+bstSearch.find(root,78));
-////        ---------------------------------Find minimum element:BSTSearch ---------------------
-//        int min= bstSearch.findMinimumElement(node);
-//        System.out.println("minimum value= "+min);
         ShortestPath shortestPath =new ShortestPath();
         int x=shortestPath.FindShortestPath(root,2,10);
         System.out.println(x);
